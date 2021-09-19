660abd994a1d48edd395753019c92407ec9e8aaf
Bregy Malpartida <bregy@minsky.cc>
2021-09-19 16:03:25 -0500

    feat: added basic example of use

---
+++ example/basic/main.go
@@ -0,0 +1,29 @@
+package main
+
+import hasura "github.com/minskylab/ent-hasura"
+
+const defaultSchemaPath = "./ent/schema"
+
+func main() {
+	input := ""
+
+	output := "metadata.json"
+	source := "default"
+
+	overrideTables := false
+	schemaName := "public"
+
+	light := false
+	defaultRole := ""
+
+	hasura.GenerateHasuraConfigurationAndRelationships(
+		defaultSchemaPath,
+		output,
+		input,
+		source,
+		schemaName,
+		overrideTables,
+		light,
+		defaultRole,
+	)
+}
