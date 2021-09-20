224c715aa65165ec953ace1714360f3a1149a3b9
Bregy Malpartida <bregy@minsky.cc>
2021-09-19 20:10:25 -0500

    feat: added default configuration generation

--- hasura.go
+++ hasura.go
@@ -1,1 +1,37 @@
 package hasura
+
+type HasuraMetadataConfig struct {
+	SchemaPath string
+	SchemaName string
+	Source     string
+
+	OutputMetadataFile string
+
+	MetadataInput  string
+	OverrideTables bool
+	DefaultRole    string
+}
+
+var DefaultHasuraMetadataConfig HasuraMetadataConfig = HasuraMetadataConfig{
+	SchemaPath: "./ent/schema",
+	SchemaName: "public",
+	Source:     "default",
+
+	OutputMetadataFile: "hasura/metadata.json",
+
+	MetadataInput:  "",
+	OverrideTables: false,
+	DefaultRole:    "",
+}
+
+func CreateDefaultMetadataFromSchema(config *HasuraMetadataConfig) error {
+	return GenerateHasuraConfigurationAndRelationships(
+		config.SchemaPath,
+		config.OutputMetadataFile,
+		config.MetadataInput,
+		config.Source,
+		config.SchemaName,
+		config.OverrideTables,
+		config.DefaultRole,
+	)
+}
