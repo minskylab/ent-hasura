22a5d1c6d1eeb0d3fe96fa7a7a9f013195babb82
Bregy Malpartida <bregy@minsky.cc>
2022-01-09 00:33:27 -0500

    feat: added relationships rename

--- apply.go
+++ apply.go
@@ -9,7 +9,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-const pgTableCustomizationAction = "pg_set_table_customization"
+const (
+	pgTableCustomizationAction = "pg_set_table_customization"
+	pgTableRenameRelationship  = "pg_rename_relationship"
+)
 
 type PGTableCustomizationArgs struct {
 	Table         string        `json:"table"`
@@ -45,6 +48,49 @@ func (r *EphemeralRuntime) setPGTableCustomization(table TableDefinition, source
 	return nil
 }
 
+type PGTableRenameArgs struct {
+	Table   string `json:"table"`
+	Name    string `json:"name"`
+	Source  string `json:"source"`
+	NewName string `json:"new_name"`
+}
+
+func (r *EphemeralRuntime) renamePGTableRelationshipsQuery(table TableDefinition, sourceName string, newName string) {
+	endpoint := fmt.Sprintf("%s/v1/metadata", r.Config.Endpoint)
+
+	res, err := r.Client.R().
+		SetHeaders(map[string]string{
+			"Content-Type":          "application/json",
+			"X-Hasura-Role":         "admin",
+			"X-Hasura-Admin-Secret": r.AdminSecret,
+		}).
+		SetBody(ActionBody{
+			Type: pgTableRenameRelationship,
+			Args: PGTableRenameArgs{
+				Table:  table.Table.Name,
+				Source: sourceName,
+				Name:   newName,
+			},
+		}).
+		Post(endpoint)
+	if err != nil {
+		logrus.Warn(errors.WithStack(err))
+		logrus.Warn("response code: ", res.StatusCode())
+	}
+}
+
+func (r *EphemeralRuntime) renamePGTableRelationships(table TableDefinition, sourceName string) error {
+	for _, t := range table.ObjectRelationships {
+		r.renamePGTableRelationshipsQuery(table, sourceName, t.Name)
+	}
+
+	for _, t := range table.ArrayRelationships {
+		r.renamePGTableRelationshipsQuery(table, sourceName, t.Name)
+	}
+
+	return nil
+}
+
 func (r *EphemeralRuntime) ApplyPGTableCustomizationForAllTables(schemaRoute, schemaName, sourceName string) error {
 	graph, err := entc.LoadGraph(schemaRoute, &gen.Config{})
 	if err != nil {
@@ -59,6 +105,11 @@ func (r *EphemeralRuntime) ApplyPGTableCustomizationForAllTables(schemaRoute, sc
 	for _, table := range tables {
 		logrus.Info("pushing table customization for table: ", table.Table.Name)
 		if err := r.setPGTableCustomization(*table, sourceName); err != nil {
+			return errors.WithStack(err)
+		}
+
+		logrus.Info("pushing table relationships for table: ", table.Table.Name)
+		if err := r.renamePGTableRelationships(*table, sourceName); err != nil {
 			return errors.WithStack(err)
 		}
 	}
